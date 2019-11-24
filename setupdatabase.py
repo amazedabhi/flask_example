088f4b1e7f95c5ee7204411110ae28f04316d441
justtabhi <abhi.in12@gmail.com>
2019-11-24 23:26:51 +0530

    first commit

---
+++ setupdatabase.py
@@ -0,0 +1,22 @@
+from basic import db,Puppy
+
+#CREATES ALL THE TABLES Model --> Db Table
+db.create_all()
+
+sam = Puppy('Sammy',3)
+frank = Puppy('Frankie',4)
+
+#None
+#None
+print(sam.id)
+print(frank.id)
+
+db.session.add_all([sam,frank])
+
+# db.session.add(sam)
+# db.session.add(frank)
+
+db.session.commit()
+
+print(sam.id)
+print(frank.id)
